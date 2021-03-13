b7e63f9d918c74e296b9fb7a1b5b8d315007e363
psharpx <crivera2093@gmail.com>
2021-03-13 16:36:58 -0500

    starter-react-router: app use for examples

--- starter-react-router/src/App.old.js
+++ starter-react-router/src/App.old.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import "./App.css";
+import DataContextProvider from "./context/DataContext";
+import Contactos from "./example/Contactos";
+import Nosotros from "./example/Nosotros";
+import Informacion from "./example/Informacion";
+import UsuarioPerfil from "./example/UsuarioPerfil";
+import UsuarioPublico from "./example/UsuarioPublico";
 
-function App() {
+function App2() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataContextProvider>
+      <BrowserRouter>
+        <div className="App">
+          <header>
+            <NavLink to="/contactos" className="pr-2">
+              Contactos
+            </NavLink>
+            <Link to="/about" className="pr-2">
+              Nosotros
+            </Link>
+            <Link to="/usuario/publico" className="pr-2">
+              Acceso Publico
+            </Link>
+            <Link
+              className="pr-2"
+              to={{
+                pathname: "/informacion/christian/rivera",
+                search: "?edad=20&sexo=M",
+              }}
+              replace
+            >
+              Perfil Christian
+            </Link>
+          </header>
+
+          <div className="container mt-4">
+            <Switch>
+              <Route
+                path="/informacion/:name/:lastname"
+                component={Informacion}
+              />
+              <Route path="/contactos" component={Contactos} />
+              <Route
+                path="/about"
+                render={({ history, location, match }) => (
+                  <Nosotros
+                    somos="Empresa Rimac"
+                    history={history}
+                    location={location}
+                    match={match}
+                  />
+                )}
+              />
+              <Route path="/usuario/publico" component={UsuarioPublico} />
+              <Route path="/usuario/:name" component={UsuarioPerfil} />
+              <Route
+                path="*"
+                render={() => <div>Ruta no encontrada - 404</div>}
+              />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </DataContextProvider>
   );
 }
 
-export default App;
+export default App2;
